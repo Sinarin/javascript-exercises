2c54f12e61bf9bd1e0bfb61d7929ec6d872e4607
sinarin <alexngdragon@hotmail.com>
2022-07-27 17:06:50 -0400

    complete js exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(n1, n2) {
+if(typeof n1 !== "number" || typeof n2 !== "number" ||
+n1 < 0 || n2 < 0) return "ERROR";
+let sum = n2;
+if(n1 > n2){
+  for(; n1 > n2; n1--){
+    sum += n1;
+  }
+}
+else {
+  while(n1 < n2){
+    sum += n1;
+    n1++;
+  }
+}
+return sum;
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
