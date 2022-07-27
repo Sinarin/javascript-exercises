52bdcf7b0568b68242c4e0cf472ffe3d44514050
sinarin <alexngdragon@hotmail.com>
2022-07-27 14:47:45 -0400

    complete js exercise1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,8 @@
-const repeatString = function() {
-
+const repeatString = function(word,n) {
+  if(n<0){
+    return "ERROR";
+  }
+  else return word.repeat(n);
 };
 
 // Do not edit below this line
